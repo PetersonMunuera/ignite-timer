d9ed5713e18af7c1819026e578d080333b7c5d3e
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-21 14:53:06 -0300

    fix: clear interval and seconds passed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -58,6 +58,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -65,12 +66,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
