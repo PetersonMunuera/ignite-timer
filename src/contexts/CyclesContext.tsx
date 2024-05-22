17ce756690f2a528bdb3bbb234c6486ae730287f
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-22 11:00:31 -0300

    fix: form reset

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -52,8 +52,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function setSecondsPassed(seconds: number) {
